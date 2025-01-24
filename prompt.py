7d6d40d3a5886180375f4c570d30a1c024eaae6d
Michael Leppert <mleppert010@gmail.com>
2025-01-24 00:01:40 -0600

    creating function for loading in rag and prompt

---
+++ prompt.py
@@ -0,0 +1,12 @@
+import subprocess
+import json
+
+
+def main():
+
+    with open('prompts.json', 'r') as file:
+        prompts = json.load(file)
+    
+    print(prompts['DGA']['base_domain'])
+
+main()
