df9f5b5a0bc158b9a343a1bd4297c8bd2e3edcb8
Michael Leppert <mleppert010@gmail.com>
2025-01-24 13:39:33 -0600

    made api key not included in git repo

--- prompt.py
+++ prompt.py
@@ -3,10 +3,14 @@ import json
 import csv
 import openai
 
-openai.api_key = "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"
 ai_model = "gpt-3.5-turbo"
 
 def main(request):
+
+    with open("apikey", "r") as file:
+        key = file.read()
+
+    openai.api_key = key
 
     type = list(request.keys())[0]
 
