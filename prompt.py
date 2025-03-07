a0e3da55cb58bb6caa1b7e6006e2ad489287a9be
Michael Leppert <mleppert010@gmail.com>
2025-03-07 13:48:12 -0600

    commit

--- prompt.py
+++ prompt.py
@@ -1,42 +1,68 @@
 import subprocess
 import json
 import csv
-import openai
+from openai import OpenAI
+
+
+with open("apikey", "r") as file:
+        key = file.read()
+
+client = OpenAI(api_key=key)
 
 ai_model = "gpt-3.5-turbo"
 
-def main(request):
+test_data = ["WWW.XN--ZALGO075952-SJGB60AIGHL2I8JC3B0A2A97FTBLL0CZA.COM",
+        "WWW.XN--ZALGO003446-SJGB60AIGHL2I8JC3B0A2A97FTBLL0CZA.COM",
+        "WWW.XN--ZALGO012841-SJGB60AIGHL2I8JC3B0A2A97FTBLL0CZA.COM",
+        "WWW.XN--ZALGO029243-SJGB60AIGHL2I8JC3B0A2A97FTBLL0CZA.COM",
+        "CLIENTALALAXP.MN",
+        "CLIENTALNOTHING.ME",
+        "USERALCLICLIENT.ME",
+        "AGENTCLIENTCLIENT.ME",
+        "JSCJSCAXPCLIALLOW.ME",
+        "JSCCLIENTAGENTDISA.ME",
+        "DISAALALLOWDISALLOW.ME",
+        "QUJFVNN.TO",
+        "CRWKBMX.TW",
+        "OLKQXMAEUIWYX.XXX",
+        "BPWENCSDVRJXJI.PRO",
+        "pop.imvhhht.ru",
+        "pop.hrfomio.ru",
+        "pop.jkkjymtb.com"]
 
-    with open("apikey", "r") as file:
-        key = file.read()
+case = "DGA"
 
-    openai.api_key = key
-
-    type = list(request.keys())[0]
+def main(case, data):
 
     with open('prompts.json', 'r') as file:
         prompts = json.load(file)
     
-    with open(prompts[type]['dir'] + prompts[type]['rag_bad'], newline='') as file:
+    with open(prompts[case]['dir'] + prompts[case]['rag_bad'], newline='') as file:
         rag_bad = list(csv.reader(file))
 
-    print(rag_bad[1:])
+    with open(prompts[case]['dir'] + prompts[case]['rag_ok'], newline='') as file:
+        rag_ok = list(csv.reader(file))
 
+    api_string = prompts[case]["prompt"]+"\n"+"\n".join(data)
 
-def test_data():
-    with open('test_data.json', 'r') as file:
-        test_data = json.load(file)
-    
-    return(test_data)
+    response = open_ai_call(api_string)
+    print(response)
+
 
 def open_ai_call(prompt):
-    try:
-        response = openai.ChatCompletion.create(model=ai_model, messages=[{"role": "user", "content": prompt}])
     
-    except:
-        response = f"Error: {str(e)}"
+    try:
+        response = client.chat.completions.create(
+             model=ai_model,
+             messages=[{"role": "user", "content": prompt}
+            ]
+        )
 
-    return(response)
+        message_content = response.choices[0].message.content
 
-main(test_data())
+    except Exception as e:
+        print(f"An error occured: {e}")
+    
+    return(message_content)
 
+main(case, test_data)
