941edf1fb7c49c4049680d8a4026d4b326fd9c7d
Michael Leppert <mleppert010@gmail.com>
2025-03-09 17:32:05 -0500

    Fixed ugly max code

--- prompt.py
+++ prompt.py
@@ -2,26 +2,23 @@ import subprocess
 import json
 import csv
 from openai import OpenAI
+from together import Together
 
 
 with open("openai_apikey", "r") as file:
         openai_key = file.read()
 
+with open("togetherai_apikey", "r") as file:
+        togetherai_key = file.read()
 
 client = OpenAI(api_key=openai_key)
+client2 = Together(api_key=togetherai_key)
 
 
 
 openai_model = "gpt-4o"
 togetherai_model = "deepseek-ai/DeepSeek-V3"
 
-=======
-with open("apikey", "r") as file:
-        key = file.read()
-client = OpenAI(api_key=key)
-#client2 = Together(api_key="")
-ai_model = "gpt-4o"
->>>>>>> Stashed changes
 test_data = ["WWW.XN--ZALGO075952-SJGB60AIGHL2I8JC3B0A2A97FTBLL0CZA.COM",
         "WWW.XN--ZALGO003446-SJGB60AIGHL2I8JC3B0A2A97FTBLL0CZA.COM",
         "WWW.XN--ZALGO012841-SJGB60AIGHL2I8JC3B0A2A97FTBLL0CZA.COM",
@@ -39,31 +36,80 @@ test_data = ["WWW.XN--ZALGO075952-SJGB60AIGHL2I8JC3B0A2A97FTBLL0CZA.COM",
         "BPWENCSDVRJXJI.PRO",
         "pop.imvhhht.ru",
         "pop.hrfomio.ru",
-        "pop.jkkjymtb.com,"
+        "pop.jkkjymtb.com",
         "etotheipiplusone.net"]
-case = "DGA"
+
+case = "WLUC1"
+
+
 def main(case, data):
+    """Formats the data, appending RAG and the prompt, then calls the AI API
+
+    Args:
+        case(str) -- The appreviation for the use case (e.g. 'DGA')
+        case["str...] -- The list of strings that represent each event's information
+
+    Returns:
+        None at this point
+    """
     with open('prompts.json', 'r') as file:
         prompts = json.load(file)
+    
     with open(prompts[case]['dir'] + prompts[case]['rag_bad'], newline='') as file:
         rag_bad = list(csv.reader(file))
+
     with open(prompts[case]['dir'] + prompts[case]['rag_ok'], newline='') as file:
         rag_ok = list(csv.reader(file))
 
-    print(data)
     api_string = prompts[case]["prompt"]+"\n"+"\n".join(data)
-    print(api_string)
-    response = open_ai_call(api_string)
+
+    response = together_call(api_string)
     print(response)
-    return response
-def open_ai_call(prompt):
+    return(response)
+
+
+def openai_call(prompt):
+    """Calls OpenAI using the specified model and primpt, returns the response
+
+    Args:
+        prompt(str) the prompt string that is sent to the AI
+
+    Returns:
+        message_content(str) the response from the AI model
+    """
     try:
         response = client.chat.completions.create(
-             model=ai_model,
+             model=openai_model,
              messages=[{"role": "user", "content": prompt}
             ]
         )
+
         message_content = response.choices[0].message.content
+
     except Exception as e:
         print(f"An error occured: {e}")
+    
     return(message_content)
+
+def together_call(prompt):
+    """Calls TogetherAI using the specified model and primpt, returns the response
+
+    Args:
+        prompt(str) the prompt string that is sent to the AI
+
+    Returns:
+        message_content(str) the response from the AI model
+    """
+    try:
+        response = client2.chat.completions.create(
+            model=togetherai_model,
+            messages=[{"role": "user", "content": prompt}],
+        )
+
+        message_content = response.choices[0].message.content
+
+    except Exception as e:
+        print(f"An error occured: {e}")
+    
+    return(message_content)
+
